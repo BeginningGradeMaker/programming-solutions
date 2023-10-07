9cdf6aa61725136e3a8d9103ba64977a801b13f0
zhisu <z2552wan@uwaterloo.ca>
2023-10-07 01:26:54 -0400

    2023/09/07

---
+++ dynamic_programming/counting_towers/counting_towers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int mod = 1000000007;
+
+int main() {
+    // solution goes here
+    int t;
+    cin >> t;
+    vector<ll> nums(t);
+
+    for (int i = 0; i < t; i++) {
+        cin >> nums[i];
+    }
+    int n = *max_element(nums.begin(), nums.end());
+
+    vector<vector<ll>> dp(2, vector<ll>(n + 1));
+    dp[0][1] = 1;
+    dp[1][1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[0][i] = (dp[0][i - 1] * 2 + dp[1][i-1]) % mod;
+        dp[1][i] = (dp[1][i - 1] * 4 + dp[0][i-1]) % mod;
+    }
+
+    for (int i = 0; i < t; i++) {
+        cout << (dp[0][nums[i]] + dp[1][nums[i]]) % mod << endl;
+    }
+}
